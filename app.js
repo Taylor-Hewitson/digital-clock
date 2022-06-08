e26575b66ec8c3ee6f34239932fd4c2b94681ba4
church0mouse <church0mouse@gmail.com>
2022-06-08 16:57:37 +1000

    css animation
    was originally going to make use javascript to make the animations but that would require a lot of physics based maths. I rediscovered css animation so I decided to use that to create the tail and eyes movement.
    very proud of how it looks.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+//initial set up to get browser time
+
 function currentTime() {
     let date = new Date(); 
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
     let session = "AM";
-  
+
+//statement to create AM PM fuction
+
     if(hh == 0){
         hh = 12;
     }
@@ -12,7 +16,9 @@ function currentTime() {
         hh = hh - 12;
         session = "PM";
      }
-  
+
+//configure the appearance
+
      hh = (hh < 10) ? "0" + hh : hh;
      mm = (mm < 10) ? "0" + mm : mm;
      ss = (ss < 10) ? "0" + ss : ss;
